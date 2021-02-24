249019a1f69bce67215dc2609bd4bb2193624d71
Eugene B <diemorgenstern@gmail.com>
2021-02-23 21:32:07 -0500

    init: final checks passed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+    let initialArr = str.split('');
+
+    if ((initialArr.length % 2 !== 0) || (initialArr.length < 2)) return false;
+
+    const isOpening = (bracket) => {
+        return (bracketsConfig.filter(arrEl => arrEl[0] === bracket).length > 0);
+    };
+
+    const isClosing = (bracket) => {
+        return (bracketsConfig.filter(arrEl => arrEl[1] === bracket).length > 0);
+    };
+
+    const findPair = (bracket) => {
+        return (bracketsConfig.filter(arrEl => arrEl[1] === bracket).map(arrEl => arrEl[0]))[0];
+    }
+
+    const finalArr = initialArr.reduce((acc, curr) => {
+
+        if (isClosing(curr)) {
+            if (acc[acc.length - 1] === findPair(curr)) {
+                acc.pop();
+                return acc;
+            }
+        }
+        acc.push(curr);
+        return acc;
+
+    }, []) || ["error"];
+
+    return (finalArr.length === 0);
 }
